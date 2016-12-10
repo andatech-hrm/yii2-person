dcf8c37ee588671566d1754b28ec94bebdafbe2f
kuakling <kuakling@gmail.com>
2016-12-10 05:14:00 -0500

    gii crud

---
+++ Module.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace andahrm\person;
+
+/**
+ * person module definition class
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * @inheritdoc
+     */
+    public $controllerNamespace = 'andahrm\person\controllers';
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
