81777f149da4bff431c103790056e52afd967233
Ahamad-PC <ahamad.jedu@gmail.com>
2019-03-27 04:50:07 +0700

    connent to person

--- views/retired/index.php
+++ views/retired/index.php
@@ -28,7 +28,26 @@ $this->params['breadcrumbs'][] = $this->title;
             //'user_id',
             [
                 'attribute' => 'user_id',
-                'value' => 'user.fullname',
+                //'value' => 'user.fullname',
+                //'attribute' => 'fullname',
+                'format' => 'raw',
+                'value' => function($model) {
+                    $old_position = '';
+                    if (isset($model->lastPosition)) {
+                        $old_position = $model->lastPosition->title;
+                    }
+                    if (isset($model->user)) {
+                        $res = '<div class="media"> <div class="media-left"> ' .
+                                '<img class="media-object img-circle" src="' . $model->user->photo . '" style="width: 32px; height: 32px;"> </div> ' .
+                                '<div class="media-body"> ' .
+                                '<h4 class="media-heading" style="margin:0;">' .
+                                Html::a($model->user->fullname, ['/person/default/view', 'id' => $model->user->user_id], ['class' => 'green', 'data-pjax' => 0]) . '</h4> ' .
+                                '<small>' . $old_position . '<small></div> </div>';
+                        return $res;
+                    } else {
+                        return $model->user->fullname;
+                    }
+                }
             ],
             [
                 'attribute' => 'last_position_id',
